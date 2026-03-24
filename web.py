ffc18b76030b8d7398a55474810c3de835664458
sirCharlesJohnson <johcha7@yahoo.com>
2026-03-24 04:17:19 -0400

    Implement "Add new todo items"

--- web.py
+++ web.py
@@ -4,9 +4,12 @@ import functions
 todos = functions.get_todos()
 
 def add_todo():
-    todo = st.session_state["new_todo"] + "\n"
-    todos.append(todo)
+    new_todo = (st.session_state.get("new_todo") or "").strip()
+    if not new_todo:
+        return
+    todos.append(new_todo + "\n")
     functions.write_todos(todos)
+    st.session_state["new_todo"] = ""
 
 
 st.title("My Todo App")
@@ -14,13 +17,16 @@ st.subheader("This is my todo app.")
 st.write("This app is to increase your productivity.")
 
 
+checked_indices = []
 for index, todo in enumerate(todos):
-    checkbox = st.checkbox(todo, key=todo)
+    checkbox = st.checkbox(todo.strip(), key=f"todo_{index}")
     if checkbox:
-        todos.pop(index)
-        functions.write_todos(todos)
-        del st.session_state[todo]
-        st.experimental_rerun()
+        checked_indices.append(index)
+
+if checked_indices:
+    todos = [t for i, t in enumerate(todos) if i not in set(checked_indices)]
+    functions.write_todos(todos)
+    st.rerun()
 
 st.text_input(label="", placeholder="Add new todo...",
               on_change=add_todo, key='new_todo')
