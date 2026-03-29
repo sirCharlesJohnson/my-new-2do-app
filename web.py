b2b2b0549089f5d7b922a85f97b3a8058fe48887
sirCharlesJohnson <johcha7@yahoo.com>
2026-03-29 07:47:32 -0400

    Add Configuration files

--- web.py
+++ web.py
@@ -16,7 +16,6 @@ st.title("My Todo App")
 st.subheader("This is my todo app.")
 st.write("This app is to increase your productivity.")
 
-
 checked_indices = []
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo.strip(), key=f"todo_{index}")
